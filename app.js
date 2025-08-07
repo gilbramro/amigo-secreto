cc7bebd782ca53993cbbb193d475100e0f3a4882
Gilbramro <gilramrom@gmail.com>
2025-08-07 09:29:49 -0500

    Mostrar y actualizar lista de amigos

--- app.js
+++ app.js
@@ -11,5 +11,25 @@ function agregarAmigo() {
     } else {
         amigos.push(ingresarAmigo);
         document.querySelector('#amigo').value = '';
+        mostrarListaAmigos();
+        limpiarCampoEntrada();
+    }
+}
+//limpiar campo de entrada
+function limpiarCampoEntrada() {
+    document.querySelector('#amigo').value = '';
+}
+
+//mostrar y actualizar lista de amigos
+function mostrarListaAmigos() {
+    let listaAmigos = document.querySelector('#listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        const li = amigos[i];
+
+        let listaHTML = document.createElement('li');
+        listaHTML.textContent = li;
+        listaAmigos.appendChild(listaHTML);
     }
 }
