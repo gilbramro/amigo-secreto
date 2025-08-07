094eb72211c7d62fad9667fc179d1b1f1cc6cbcb
Gilbramro <gilramrom@gmail.com>
2025-08-07 09:07:33 -0500

    Declaración variable Captura y validación

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Challenge Amigo Secreto
+//Declaración de variables
+let amigos = [];
+//Capturar valor de entrada
+function agregarAmigo() {
+    let ingresarAmigo = document.getElementById('amigo').value;
+//validación de entrada y método push
+    if (ingresarAmigo === '') {
+     alert("Por favor ingrese un nombre:");
+     //console.log(ingresarAmigo);
+    } else {
+        amigos.push(ingresarAmigo);
+        document.querySelector('#amigo').value = '';
+    }
+}
